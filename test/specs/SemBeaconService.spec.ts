234c8c2e65b286ac96dfb7ca87d14ec1b89d6fa3
Maxim Van de Wynckel <maxim.van.de.wynckel@vub.be>
2024-02-19 13:59:58 +0100

    test: additional unit tests

--- test/specs/SemBeaconService.spec.ts
+++ test/specs/SemBeaconService.spec.ts
@@ -15,6 +15,24 @@ describe('SemBeaconService', () => {
         service.emitAsync('build').then(() => done()).catch(done);
     });
 
+    describe('insert()', () => {
+        it('should resolve a sembeacon and all related beacons', (done) => {
+            BLESemBeaconBuilder.create()
+                .namespaceId(BLEUUID.fromString('77f340db-ac0d-20e8-aa3a-f656a29f236c'))
+                .instanceId('9c7ce6fc')
+                .calibratedRSSI(-56)
+                .shortResourceUri('https://bit.ly/3JsEnF9')
+                .build().then(beacon => {
+                    return service.insert(beacon.uid, beacon);
+                }).then(insertedObject => {
+                    expect(insertedObject.position).to.not.be.undefined;
+                    expect(insertedObject.position.toVector3().x).to.not.be.undefined;
+                    expect(insertedObject.position.toVector3().x).to.not.eq(0);
+                    done();
+                }).catch(done);
+        });
+    });
+
     describe('resolve()', () => {
         it('should resolve all beacons', (done) => {
             BLESemBeaconBuilder.create()
@@ -30,6 +48,8 @@ describe('SemBeaconService', () => {
                     result.beacons.forEach(beacon => {  
                         expect(beacon).to.not.be.undefined;
                         expect(beacon.position).to.not.be.undefined;
+                        expect(beacon.position.toVector3().x).to.not.be.undefined;
+                        expect(beacon.position.toVector3().x).to.not.eq(0);
                     });
                     done();
                 }).catch(done);
@@ -49,20 +69,6 @@ describe('SemBeaconService', () => {
                     expect(deserialized.data).to.not.be.undefined;
                     const store = new Store(deserialized.data);
                     expect(store.size).to.be.greaterThan(10);
-                    done();
-                }).catch(done);
-        });
-
-        it('should resolve a sembeacon', (done) => {
-            BLESemBeaconBuilder.create()
-                .namespaceId(BLEUUID.fromString('77f340db-ac0d-20e8-aa3a-f656a29f236c'))
-                .instanceId('9c7ce6fc')
-                .calibratedRSSI(-56)
-                .shortResourceUri('https://bit.ly/3JsEnF9')
-                .build().then(beacon => {
-                    return service.insert(beacon.uid, beacon);
-                }).then(insertedObject => {
-                    expect(insertedObject.position).to.not.be.undefined;
                     done();
                 }).catch(done);
         });
